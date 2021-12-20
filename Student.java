bfafe06451913096969669e5a199ff70dd5fe21d
poojanidoni <poojanidonixworksz@gmail.com>
2021-12-20 17:21:38 +0530

    hello

---
+++ Student.java
@@ -0,0 +1,9 @@
+C:\Users\HP\Documents\Nov 22nd classesclass Student{
+	public static void main(String []a)
+	{
+	byte [] ids={3,4,5,2,5,4};
+	System.out.println(ids[2]);
+	System.out.println(ids[4]);
+	}
+}
+	
